e16268e84d282188d6cfa6a148398dd50b0b6249
Michal Špaček <tupinek@gmail.com>
2014-11-02 15:15:02 +0100

    Implement own report() method with array and scalar context and returning only 2 last records.

--- Process.pm
+++ Process.pm
@@ -8,6 +8,15 @@ use warnings;
 # Version.
 our $VERSION = 0.01;
 
+# Get report.
+sub report {
+	my $self = shift;
+	my $report = $self->SUPER::report;
+	my @report_full = split m/\n/ms, $report;
+	my @report = ($report_full[-2], $report_full[-1]);
+	return wantarray ? @report : (join "\n", @report);
+}
+
 1;
 
 __END__
