8302de41d208bf035e916c68b1233c600160a4bc
Michal Špaček <tupinek@gmail.com>
2014-11-07 12:27:30 +0100

    Prepare for 0.02 version.

--- Process.pm
+++ Process.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Get report.
 sub report {
@@ -121,6 +121,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
