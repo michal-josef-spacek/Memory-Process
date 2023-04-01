97e33822d5b367de05f2c40b34a1ca13aa33a983
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-04-01 13:14:10 +0200

    Prepare for 0.07 version

--- Process.pm
+++ Process.pm
@@ -9,7 +9,7 @@ use Readonly;
 # Constants.
 Readonly::Scalar our $EMPTY_STR => q{};
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Record.
 sub record {
@@ -266,6 +266,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
